5a722a818a3c02b969034b8eb6a764a53b684560
Geneviève Cirera <gcirera1@ypg.com>
2018-07-10 14:10:46 -0400

    Add products test

--- src/core/object.py
+++ src/core/object.py
@@ -1,3 +1,6 @@
 class Object(object):
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
---
+++ src/products/products_test.py
@@ -0,0 +1,29 @@
+from unittest.mock import MagicMock
+from ..core import Object
+
+from .products import Products
+import config
+
+test_config = config.get_config('test')
+
+
+def test_get_product_empty():
+    db = MagicMock()
+    db.find.return_value = []
+    products = Products(test_config, MagicMock(), { 'test': db })
+    result = products.get_products(10, 0)
+    db.find.assert_called_once_with(skip=0, limit=10)
+    assert len(result.products) == 0
+    assert result.total == 0
+
+
+def test_get_product_with_result():
+    db = MagicMock()
+    data = { 'product_name': 'test', 'ingredients': [{ 'id': 'id_test', 'text': 'test' }] }
+    db.find.return_value = [data]
+    products = Products(test_config, MagicMock(), { 'test': db })
+    result = products.get_products(10, 0)
+    db.find.assert_called_once_with(skip=0, limit=10)
+    assert len(result.products) == 1
+    assert result.products[0] == Object(name='test', ingredients=[Object(id='id_test', name='test')])
+    assert result.total == 1
